ba0157c963d782ab9663e5070f0152d0f94fb342
juhwano <calculus9006@gmail.com>
2024-12-01 16:12:29 +0900

    날씨다이어리 Delete 구현

--- src/main/java/zerobase/weather/repository/DiaryRepository.java
+++ src/main/java/zerobase/weather/repository/DiaryRepository.java
@@ -2,6 +2,7 @@ package zerobase.weather.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import zerobase.weather.domain.Diary;
 
 import java.time.LocalDate;
@@ -14,4 +15,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Integer> {
     List<Diary> findByDateBetween(LocalDate startDate, LocalDate endDate);
 
     Diary getFirstByDate(LocalDate data); // getFirst = Limit(1)
+
+    @Transactional // 붙여야 삭제됨
+    void deleteAllByDate(LocalDate date);
 }
